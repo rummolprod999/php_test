13c091fedd51762ffa11b13e1d75dcc6945a6805
rummolprod999 <rummolprod999@gmail.com>
2016-12-01 10:42:07 +0300

    обновление

--- xml_test.php
+++ xml_test.php
@@ -12,4 +12,5 @@ $file = file_get_contents('0000000025_purchaseNoticeEP_20161107_115924_002.xml')
 $xml = new SimpleXMLElement($file);
 //echo $xml->asXML();
 readfile('0000000025_purchaseNoticeEP_20161107_115924_002.xml');
+?>
 
