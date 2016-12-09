c63a0a6cb56e472d71ab31b5e383eb51be649072
rummolprod999 <rummolprod999@gmail.com>
2016-12-09 17:27:27 +0300

    обновление

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 09.12.16
+ * Time: 17:17
+ */
+$a = 4;
+if ($a == 5) { ?>
+    <p>A равно 5</p>
+<?php ?>
+<?php } else { ?>
+    <p>A не равно 5</p>
+<?php } ?>
